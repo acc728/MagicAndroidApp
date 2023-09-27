26d75ea48ae765af531225387f91dce05a5ccdb2
Adrian Camacho Capilla <adrian.cam.cap@gmail.com>
2023-09-27 11:53:44 +0200

    Dependency injection implemented

---
+++ app/src/main/java/com/hiberus/magicandroidapp/MagicApplication.kt
@@ -0,0 +1,20 @@
+package com.hiberus.magicandroidapp
+
+import android.app.Application
+import com.hiberus.magicandroidapp.di.baseModule
+import com.hiberus.magicandroidapp.di.cardsModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MagicApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MagicApplication)
+            modules(listOf(baseModule, cardsModule)).allowOverride(true)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/hiberus/magicandroidapp/di/modules.kt
@@ -0,0 +1,43 @@
+package com.hiberus.magicandroidapp.di
+
+import androidx.room.Room
+import com.hiberus.magicandroidapp.data.card.CardDataImpl
+import com.hiberus.magicandroidapp.data.card.local.CardLocalImpl
+import com.hiberus.magicandroidapp.data.card.remote.CardRemoteImpl
+import com.hiberus.magicandroidapp.data.database.AppDatabase
+import com.hiberus.magicandroidapp.data.remote.ApiClient
+import com.hiberus.magicandroidapp.data.remote.CardMagicService
+import com.hiberus.magicandroidapp.domain.CardRepository
+import com.hiberus.magicandroidapp.domain.usecases.AddCardUseCase
+import com.hiberus.magicandroidapp.domain.usecases.DeleteCardUseCase
+import com.hiberus.magicandroidapp.domain.usecases.EditCardUseCase
+import com.hiberus.magicandroidapp.domain.usecases.GetCardAutocompleteUseCase
+import com.hiberus.magicandroidapp.domain.usecases.GetCardUseCase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val baseModule = module {
+    single<AppDatabase> {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java, "cards_db"
+        ).build()
+    }
+
+    single<CardMagicService> { ApiClient.retrofit.create(CardMagicService::class.java) }
+}
+
+val cardsModule = module {
+    factory { CardLocalImpl(get()) }
+    factory { CardRemoteImpl(get()) }
+
+    factory<CardRepository> { CardDataImpl(get(), get()) }
+
+    factory { AddCardUseCase(get()) }
+    factory { DeleteCardUseCase(get()) }
+    factory { EditCardUseCase(get()) }
+    factory { GetCardUseCase(get()) }
+    factory { GetCardAutocompleteUseCase(get()) }
+
+    //viewModel { NotesViewModel(get(), get(), get(), get(), get()) }
+}
